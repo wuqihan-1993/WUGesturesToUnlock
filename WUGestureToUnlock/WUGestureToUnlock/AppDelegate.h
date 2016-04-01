dc9907a5864fc61dec12b69f4843463801a8ae14
wuqh <wuqh@wuqhdeMacBook-Pro.local>
2016-04-01 19:23:43 +0800

    初始化

---
+++ WUGestureToUnlock/WUGestureToUnlock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WUGestureToUnlock
+//
+//  Created by wuqh on 16/4/1.
+//  Copyright © 2016年 吴启晗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WUGestureToUnlock/WUGestureToUnlock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WUGestureToUnlock
+//
+//  Created by wuqh on 16/4/1.
+//  Copyright © 2016年 吴启晗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
